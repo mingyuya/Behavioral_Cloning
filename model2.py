1784b8eda840611b63465811c5ca624bfb141eea
Matt Kim <mingyuya@gmail.com>
2017-03-23 09:27:41 +0900

    new model

--- model2.py
+++ model2.py
@@ -2,6 +2,7 @@ import os
 import csv
 import cv2
 import numpy as np
+from sklearn.utils import shuffle
 
 samples = []
 with open('./record/driving_log.csv') as csvfile:
@@ -9,10 +10,10 @@ with open('./record/driving_log.csv') as csvfile:
     for line in reader:
         samples.append(line)
 
+samples = shuffle(samples)
 from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_samples, validation_samples = train_test_split(samples, test_size=0.3)
 
-from sklearn.utils import shuffle
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -31,9 +32,7 @@ def generator(samples, batch_size=32):
                 images.append(center_image)
                 angles.append(center_angle)
 
-            # trim image - 70 rows from the top, 20 rows from the bottom
             X_train = np.array(images)
-            #X_train = np.array(images[20:89,:,:])
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
@@ -42,7 +41,7 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Activation
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Activation, Dropout
 from keras.layers.normalization import BatchNormalization
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
@@ -50,7 +49,7 @@ from keras.optimizers import Adam
 model = Sequential()
 
 # Nvidia's Model with Batch Normalization
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 127.5) - 1., input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,20),(0,0)), input_shape=(160, 320, 3)))
 model.add(BatchNormalization())
 model.add(Conv2D(24, 5, 5, subsample=(2,2)))
@@ -67,8 +66,10 @@ model.add(BatchNormalization())
 model.add(Conv2D(64, 3, 3))
 model.add(BatchNormalization())
 model.add(Flatten())
+model.add(Dropout(0.8))
 model.add(Dense(100))
 model.add(BatchNormalization())
+#model.add(Dropout(0.9))
 model.add(Dense(50))
 model.add(BatchNormalization())
 model.add(Dense(10))
@@ -81,8 +82,9 @@ model.compile(loss='mse', optimizer=adam)
 history_object = model.fit_generator(train_generator, 
                                      validation_data = validation_generator,
                                      samples_per_epoch = len(train_samples),
+                                     #samples_per_epoch = int(len(train_samples)*0.8),
                                      nb_val_samples = len(validation_samples), 
-                                     nb_epoch=8,
+                                     nb_epoch=5,
                                      verbose=1)
 
 model.save('model.h5')
